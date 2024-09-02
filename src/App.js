b97d1ed0ef93fb376fbc45d997fb208594f72cc7
Tim Pugh <nwtpugh@gmail.com>
2024-09-02 13:43:47 -0700

    Update frontend to call specific backend APIs

--- src/App.js
+++ src/App.js
@@ -5,10 +5,80 @@ function App() {
   const [showResult, setShowResult] = useState(false);
   const [apiMessage, setApiMessage] = useState("");
 
-  const getApiMessage = async () => {
+  const helloWorld = async () => {
 
     console.log(process.env.REACT_APP_ENDPOINT);
     const response = await fetch(`${process.env.REACT_APP_ENDPOINT}hello`, {
+      mode: 'cors'
+    });
+    
+    const responseData = await response.text();
+    console.log(responseData)
+
+    setShowResult(true);
+    setApiMessage(responseData);  
+  };
+
+  const createPlayer = async () => {
+
+    console.log(process.env.REACT_APP_ENDPOINT);
+    const response = await fetch(`${process.env.REACT_APP_ENDPOINT}players`, {
+      mode: 'cors'
+    });
+    
+    const responseData = await response.text();
+    console.log(responseData)
+
+    setShowResult(true);
+    setApiMessage(responseData);  
+  };
+
+  const getPlayer = async () => {
+
+    console.log(process.env.REACT_APP_ENDPOINT);
+    const response = await fetch(`${process.env.REACT_APP_ENDPOINT}players/{id}`, {
+      mode: 'cors'
+    });
+    
+    const responseData = await response.text();
+    console.log(responseData)
+
+    setShowResult(true);
+    setApiMessage(responseData);  
+  };
+
+  const listPlayers = async () => {
+
+    console.log(process.env.REACT_APP_ENDPOINT);
+    const response = await fetch(`${process.env.REACT_APP_ENDPOINT}players`, {
+      mode: 'cors'
+    });
+    
+    const responseData = await response.text();
+    console.log(responseData)
+
+    setShowResult(true);
+    setApiMessage(responseData);  
+  };
+
+  const updatePlayer = async () => {
+
+    console.log(process.env.REACT_APP_ENDPOINT);
+    const response = await fetch(`${process.env.REACT_APP_ENDPOINT}players/{id}/{date}`, {
+      mode: 'cors'
+    });
+    
+    const responseData = await response.text();
+    console.log(responseData)
+
+    setShowResult(true);
+    setApiMessage(responseData);  
+  };
+
+  const deletePlayer = async () => {
+
+    console.log(process.env.REACT_APP_ENDPOINT);
+    const response = await fetch(`${process.env.REACT_APP_ENDPOINT}players/{id}/{date}`, {
       mode: 'cors'
     });
     
@@ -24,10 +94,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>CALL AN API</h1>
-        <button onClick={getApiMessage}>Call Kinesis Producer</button>
-        <button onClick={getApiMessage}>Call DynamoDB Producer</button>
-        <button onClick={getApiMessage}>Call SNS Producer</button>
-        <button onClick={getApiMessage}>Call SQS Producer</button>
+        <button onClick={helloWorld}>Call Hello World</button>
+        <button onClick={createPlayer}>Call Create Player</button>
+        <button onClick={getPlayer}>Call Get Player</button>
+        <button onClick={listPlayers}>Call List Players</button>
+        <button onClick={updatePlayer}>Call Update Player</button>
+        <button onClick={deletePlayer}>Call Delete Player</button>
         <div>
           {showResult && <code>{JSON.stringify(apiMessage, null, 2)}</code>}
         </div>
